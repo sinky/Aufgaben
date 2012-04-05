26c4e54a40a195986c6614883fd0becc4b7bb465
Marco <marco@my-azur.de>
2012-04-05 11:14:43 +0000

    added config

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+define('DBSERVER',  'your_db_server');
+define('DBNAME',    'your_db_name');
+define('DBUSER',    'your_db_user');
+define('DBPASS',    'your_db_password');
+
+define('DBTABLE',   'aufgaben');
